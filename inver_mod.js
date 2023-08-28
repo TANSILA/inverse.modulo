3c0172892254bfbb18c76cb0057a3f2b666b455b
Vigneshwaran.H <72210385+TANSILMS@users.noreply.github.com>
2023-08-28 03:35:41 -0600

    Update inver_mod.js

--- inver_mod.js
+++ inver_mod.js
@@ -3,17 +3,18 @@ function myfunc()
     const variable1 = document.getElementById("yoh").value;
     const variable2 = document.getElementById("yuh").value;
     let i=1;
+    let multi;
     for(i=1;i<=50;i++)
     {
-     let multi = (variable1 * i);
+     multi = (variable1 * i);
      if(multi % variable2==1)
      {
-        alert("the possible integer to get modulo value = 1 for your inverse is : " + i);
+        alert("The possible integer to get modulo value = 1 for your inverse is : " + i);
         break;
      }
-        else{
-            alert("This equation has no solution!<br>
-This is because"+ variable1 "and" + variable2 "aren't coprime")
     }
+    if(multi % variable2 != 1)
+   {
+    alert(`This equation has no solution!\n Because ${variable1} and ${variable2} are co-primes`);
+   }
 }
-    }
